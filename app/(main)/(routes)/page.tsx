c40e523472684e559efc845374c62b7882e1f1f0
ankit singh <ankiitsingh21@gmail.com>
2025-07-30 15:45:37 +0530

    changed a little bit colour in a dark mode theme ui

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mod-toggle";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/sign-in" />
+      <ModeToggle/>
     </div>
   );
 }
