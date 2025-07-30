cb93af6c1a7af5069aacde5042c0d7423f8f8456
ankit singh <ankiitsingh21@gmail.com>
2025-07-31 02:21:22 +0530

    INitial Modal ui has been done

---
+++ lib/initial-profile.ts
@@ -0,0 +1,32 @@
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");  // SERVER-SIDE REDIRECT
+  }
+
+  let profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  // Create new profile if none exists
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName ?? ""} ${user.lastName ?? ""}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0]?.emailAddress
+    },
+  });
+
+  return newProfile;
+};
