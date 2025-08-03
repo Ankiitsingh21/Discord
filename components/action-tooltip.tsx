d4d07c8c832999d0f9655038dac4725650462ddf
ankit singh <ankiitsingh21@gmail.com>
2025-08-03 18:34:16 +0530

    completed navigation sidebar

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Tooltip,
+        TooltipContent,
+        TooltipProvider,
+        TooltipTrigger
+ } from "@/components/ui/tooltip"
+import { Children } from "react";
+
+ interface ActionToolTipProps{
+        label:string;
+        children :React.ReactNode;
+        side?: "top" | "right" | "bottom" | "left";
+        align?: "start" | "center" | "end";
+ }
+
+ export const ActionToolTip=({
+        label,
+        children,
+        side,
+        align,
+
+ }:ActionToolTipProps)=>{
+        return (
+                <TooltipProvider>
+                        <Tooltip delayDuration={50} >
+                            <TooltipTrigger>
+                                {children}
+                            </TooltipTrigger>  
+                            <TooltipContent side={side} align ={align} >
+                                <p className="font-semibold text-sm capitalize" >
+                                        {label.toLowerCase()}
+                                </p>
+                            </TooltipContent>  
+                        </Tooltip>
+                </TooltipProvider>
+        )
+ }
