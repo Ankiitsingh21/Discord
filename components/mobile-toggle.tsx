baa5cec19a93cdc092cbc46bec19faec2f9e7fe1
ankit singh <ankiitsingh21@gmail.com>
2025-08-07 20:41:58 +0530

    fixed chat header error

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,34 +1,32 @@
 import { Ghost, Menu } from "lucide-react"
-import { Button } from "./ui/button"
-
 import {
-        Sheet,
-        SheetContent,
-        SheetTrigger
+    Sheet,
+    SheetContent,
+    SheetTrigger
 } from "@/components/ui/sheet"
+import { Button } from "./ui/button"
 import { NavigationSideBar } from "./navigation/navigation-sidebar"
-import { ServerSideBar } from "./server/server-sidebar"
+import {ServerSideBar} from "./server/server-sidebar"
 
-export const MobileToggle = ({
-        serverId
-}:{
-        serverId:string
+export const MobileToggle =({serverId}:{
+    serverId:string
 })=>{
-        return (
-                <div>
-                       <Sheet>
-                        <SheetTrigger>
-                               <Button variant = "ghost" size="icon" className="md:hidden" >
-                                  <Menu/>
-                                </Button> 
-                        </SheetTrigger>
-                        <SheetContent side="left" className="p-0 flex gap-0" >
-                                <div className="w-[72px]" >
-                                        <NavigationSideBar/>
-                                </div>
-                                <ServerSideBar serverId={serverId} />
-                        </SheetContent>
-                       </Sheet>
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon"
+                className="md:hidden">
+                    <Menu/>
+                </Button>
+            </SheetTrigger>
+
+            <SheetContent side="left" 
+            className="p-0 flex gap-0">
+                <div className="w-[72px]">
+                    <NavigationSideBar/>
                 </div>
-        )
+                <ServerSideBar serverId={serverId}/>
+            </SheetContent>
+        </Sheet>
+    )
 }
