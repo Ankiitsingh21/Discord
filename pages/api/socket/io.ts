929d6a508ddcadbe039217f1bca63d15581b463e
ankit singh <ankiitsingh21@gmail.com>
2025-08-13 12:53:01 +0530

    fixed version uploaded

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,8 +1,7 @@
 import { Server as NetServer } from "http";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest } from "next";
 import { Server as ServerIO } from "socket.io";
-
-import { NextApiResponseIo } from "@/types";
+import type { NextApiResponseIo } from "@/types";
 
 export const config = {
   api: {
@@ -10,19 +9,28 @@ export const config = {
   },
 };
 
-const ioHandler = (_req: NextApiRequest, res: NextApiResponseIo) => {
+export default function ioHandler(
+  _req: NextApiRequest,
+  res: NextApiResponseIo
+) {
+  // Make sure the socket server exists before initializing IO
+  if (!res.socket?.server) {
+    console.error("Socket server is not ready yet.");
+    res.status(500).end();
+    return;
+  }
+
   if (!res.socket.server.io) {
-    const path = "/api/socket/io";
+    console.log("Starting new Socket.io server...");
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
-      path: path,
+      path: "/api/socket/io",
       addTrailingSlash: false,
     });
-
     res.socket.server.io = io;
+  } else {
+    console.log("Socket.io server already running.");
   }
 
   res.end();
-};
-
-export default ioHandler;
+}
